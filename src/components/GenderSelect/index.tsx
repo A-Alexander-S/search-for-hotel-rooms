7b8faf70a6bf082ab9ea1ca4b2e95667700e422e
Alexander <impoossible.777@gmail.com>
2022-04-19 17:45:52 +0400

    c-nt GenderSelect removed subtitle

--- src/components/GenderSelect/index.tsx
+++ src/components/GenderSelect/index.tsx
@@ -7,13 +7,28 @@ export default class GenderSelect extends React.Component {
       <div className="gender-select" >
         <div className="gender-select__btns">
           <div className="gender-select__btns-gender">
-            <input id="gender-select__man" className="gender-select__input" type="radio" name="gender" value="man" />
-            <label className="gender-select__label" htmlFor="gender-select__man"></label>
+            <input
+              id="gender-select__man"
+              className="gender-select__input"
+              type="radio"
+              name="gender"
+              value="man"
+              checked />
+            <label
+              className="gender-select__label"
+              htmlFor="gender-select__man"></label>
             <p>Мужчина</p>
           </div>
           <div className="gender-select__btns-gender">
-            <input id="gender-select__female" className="gender-select__input" type="radio" name="gender" value="female" />
-            <label className="gender-select__label" htmlFor="gender-select__female"></label>
+            <input
+              id="gender-select__female"
+              className="gender-select__input"
+              type="radio"
+              name="gender"
+              value="female" />
+            <label
+              className="gender-select__label"
+              htmlFor="gender-select__female"></label>
             <p>Женщина</p>
           </div>
         </div>
