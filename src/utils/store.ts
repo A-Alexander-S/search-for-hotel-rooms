937c5f73c7ecf45a40fb35f24bf2d4faec650f5e
Alexander <impoossible.777@gmail.com>
2022-06-07 15:46:58 +0400

    setting redux-thunk

--- src/utils/store.ts
+++ src/utils/store.ts
@@ -1,12 +1,17 @@
-import { createStore } from 'redux';
-import initReducers from '../reducers';
+import { createStore, compose, applyMiddleware } from 'redux';
+import thunkMiddleware from 'redux-thunk'
+import thunk from 'redux-thunk';
+import rootReducers from '../reducers'; //initReducers
+
+const composeEnhancers = (window as any).__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 function initStore() {
   const initialStore = {};
 
   return createStore(
-    initReducers,
+    rootReducers,
     initialStore,
+    composeEnhancers(applyMiddleware(thunk))
   );
 }
 
