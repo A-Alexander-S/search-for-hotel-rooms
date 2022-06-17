d94e797848b76c9cc61f8bae1279b72dd328c356
Alexander <impoossible.777@gmail.com>
2022-06-17 15:49:46 +0400

    RoomCard: configured to upload images from the server

---
+++ src/utils/constants.ts
@@ -0,0 +1,9 @@
+export const GET_ROOMS_ALL_URL = 'http://localhost:3000/rooms/api/all';
+export const SERVER_ROOT_URL = 'http://localhost:3000/';
+
+export const enum FETCH_STATUSES {
+  idle = 'idle',
+  request = 'request',
+  success = 'success',
+  failure = 'failure'
+}
