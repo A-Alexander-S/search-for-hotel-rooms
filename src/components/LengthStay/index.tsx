a07b65768e9a22427839c41b0c61f0869c940608
Alexander <impoossible.777@gmail.com>
2022-07-01 15:11:22 +0400

    c-nt LengthStay connected with store

--- src/components/LengthStay/index.tsx
+++ src/components/LengthStay/index.tsx
@@ -1,59 +1,68 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import {
+  changeDateArrivalAction,
+  changeDateArrivalType,
+  changeDateDepartureAction,
+  changeDateDepartureType
+} from '../../actions/filterRoomsActions';
 import './length-stay.scss';
 
 interface ILengthStayProps {
   options: {
     dateArrivalTitle: string,
-    dateArrivalPlaceholder?: string,
     dateDepartureTitle: string,
-    dateDeparturePlaceholder?: string,
-    handleChangeInputArrival: Function,
-    handleChangeInputDeparture: Function
-  }
+  },
+  changeDateArrivalAction: (dateArrival: string) => changeDateArrivalType,
+  changeDateDepartureAction: (dateDeparture: string) => changeDateDepartureType,
 }
 
-export default class LengthStay extends React.Component<ILengthStayProps> {
+class LengthStay extends React.Component<ILengthStayProps> {
 
   constructor(props: ILengthStayProps) {
     super(props);
   }
 
-  handleChangeInputArrival = (e: React.ChangeEvent<HTMLInputElement>) => {
-    this.props.options.handleChangeInputArrival(e);
+  handleChangeDateArrival = (e: React.ChangeEvent<HTMLInputElement>) => {
+    this.props.changeDateArrivalAction(e.currentTarget.value);
   }
 
-  handleChangeInputDeparture = (e: React.ChangeEvent<HTMLInputElement>) => {
-    this.props.options.handleChangeInputDeparture(e);
+  handleChangeDateDeparture = (e: React.ChangeEvent<HTMLInputElement>) => {
+    this.props.changeDateDepartureAction(e.currentTarget.value);
   }
 
   render(): React.ReactNode {
     const {
       dateArrivalTitle,
-      dateArrivalPlaceholder,
       dateDepartureTitle,
-      dateDeparturePlaceholder
-    } = this.props.options
+    } = this.props.options;
     return (
       <div className="length-stay">
         <div className="length-stay__date-arrival">
           <p className="length-stay__arrival-title caption">{dateArrivalTitle}</p>
           <input
             className="date-arrival__dropdown length-stay__input"
-            placeholder={dateArrivalPlaceholder}
             name="date-arrival"
             type="date"
-            onChange={this.handleChangeInputArrival} />
+            onChange={this.handleChangeDateArrival} />
         </div>
         <div className="length-stay__date-departure">
           <p className="ength-stay__departure-title caption">{dateDepartureTitle}</p>
           <input
             className="date-departure__dropdown length-stay__input"
-            placeholder={dateDeparturePlaceholder}
             name="date-departure"
             type="date"
-            onChange={this.handleChangeInputDeparture} />
+            onChange={this.handleChangeDateDeparture} />
         </div>
       </div>
     );
   }
 }
+
+const mapDispatchToProps = dispatch => bindActionCreators({
+  changeDateArrivalAction,
+  changeDateDepartureAction
+}, dispatch);
+
+export default connect(null, mapDispatchToProps)(LengthStay);
