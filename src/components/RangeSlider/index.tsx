555738bb294ff885829c5a13d35b39ec5c9a95e8
Alexander <impoossible.777@gmail.com>
2022-08-01 14:04:01 +0400

    edit  RangeSlider(self-written)

--- src/components/RangeSlider/index.tsx
+++ src/components/RangeSlider/index.tsx
@@ -1,91 +1,211 @@
-import React, { LegacyRef } from "react";
-import noUiSlider from "nouislider";
+import React from "react";
+import { bindActionCreators } from "redux";
+import { connect } from "react-redux";
+import {
+  changeMaxPricePerRoomAction,
+  changeMinPricePerRoomAction,
+  changeMinPricePerRoomType,
+  changeMaxPricePerRoomType
+} from "../../actions/filterRoomsActions";
 import "./range-slider.scss";
-// import "nouislider/dist/nouislider.js";
 
-export class RangeSlider extends React.Component {
-  private rangeSlider: React.RefObject<HTMLDivElement>;
+interface IRangeSliderProps {
+  changeMinPricePerRoomAction: (minPricePerRoom: number) => changeMinPricePerRoomType,
+  changeMaxPricePerRoomAction: (maxPricePerRoom: number) => changeMaxPricePerRoomType,
+}
 
-  constructor(props) {
-    super(props)
+const priceGap = 1000;
+const step = 500;
 
-    this.rangeSlider = React.createRef<HTMLDivElement>();
+class RangeSlider extends React.Component<IRangeSliderProps>{
+
+  state = {
+    minPrice: 2500,
+    maxPrice: 7500,
+    ifMaxIsSmall: 1,
+    isSmall: false,
+    progressBarStyle: {
+      left: '25%',
+      right: '25%',
+    }
+  }
+
+  constructor(props: IRangeSliderProps) {
+    super(props);
   }
 
   /**
-   * plugin noUiSlider
+   * Change handler input with minimum price
   */
-  componentDidMount() {
-    if (this.rangeSlider) {
-      noUiSlider.create(this.rangeSlider.current, {
-        start: [4000, 15000],
-        connect: true,
-        step: 1,
-        range: {
-          'min': [4000],
-          'max': [15000]
+  handleChangeMinPriceInput = (e: React.ChangeEvent<HTMLInputElement>) => {
+
+    if (this.state.maxPrice - +e.currentTarget.value > priceGap) {
+      const percent = (+e.currentTarget.value / +e.currentTarget.max) * 100;
+      this.setState({
+        minPrice: +e.currentTarget.value,
+        progressBarStyle: {
+          ...this.state.progressBarStyle,
+          left: `${percent}%`
         }
       });
-
-      const input0 = document.getElementById('input-0');
-      const input1 = document.getElementById('input-1');
-      const inputs = [input0, input1];
-
-      // @ts-ignore
-      this.rangeSlider.current.noUiSlider.on('update', function (values, handle) {
-        //@ts-ignore
-        inputs[handle].value = Math.round(values[handle]);
-        // console.log(values)
-      });
-
-      const setRangeSlider = (i, value) => {
-        let arr = [null, null];
-        arr[i] = value;
-
-        // console.log(arr);
-        // @ts-ignore
-        this.rangeSlider.current.noUiSlider.set(arr);
-      };
-
-      inputs.forEach((el, index) => {
-        // @ts-ignore
-        el.addEventListener('change', (e) => {
-          // @ts-ignore
-          setRangeSlider(index, e.currentTarget.value);
-        });
+    }
+    if (this.state.maxPrice - +e.currentTarget.value <= priceGap
+      && +e.currentTarget.value <= (10000 - priceGap)) {
+      const percentLeft = (+e.currentTarget.value / +e.currentTarget.max) * 100;
+      const percentRight = 100 - ((+e.currentTarget.value + priceGap) / +e.currentTarget.max) * 100;
+      this.setState({
+        minPrice: +e.currentTarget.value,
+        maxPrice: +e.currentTarget.value + priceGap,
+        progressBarStyle: {
+          left: `${percentLeft}%`,
+          right: `${percentRight}%`,
+        }
       });
     }
   }
 
+  /**
+   * Change handler input with max price
+  */
+  handleChangeMaxPriceInput = (e: React.ChangeEvent<HTMLInputElement>) => {
+
+    if (+e.currentTarget.value - this.state.minPrice >= priceGap
+      && +e.currentTarget.value <= 10000) {
+      const percent = 100 - (+e.currentTarget.value / +e.currentTarget.max) * 100;
+      this.setState({
+        maxPrice: +e.currentTarget.value,
+        isSmall: false,
+        progressBarStyle: {
+          ...this.state.progressBarStyle,
+          right: `${percent}%`
+        }
+      });
+    }
+    if (+e.currentTarget.value - this.state.minPrice <= priceGap) {
+      const percent = 100 - ((this.state.minPrice + priceGap) / +e.currentTarget.max) * 100;
+      this.setState({
+        maxPrice: this.state.minPrice + priceGap,
+        ifMaxIsSmall: +e.currentTarget.value,
+        isSmall: true,
+        progressBarStyle: {
+          ...this.state.progressBarStyle,
+          right: `${percent}%`
+        }
+      });
+    }
+  }
+
+  /**
+   * Change handler thumb  minimum price
+  */
+  handleDragableThumbMin = (e: React.MouseEvent<HTMLInputElement>) => {
+    const percent = (+e.currentTarget.value / +e.currentTarget.max) * 100;
+    if (this.state.maxPrice - +e.currentTarget.value < priceGap) {
+      this.setState({
+        minPrice: this.state.maxPrice - priceGap,
+      });
+    } else {
+      this.setState({
+        minPrice: +e.currentTarget.value,
+        progressBarStyle: {
+          ...this.state.progressBarStyle,
+          left: `${percent}%`
+        }
+      });
+    }
+  }
+
+  /**
+   * Change handler thumb  max price
+  */
+  handleDragableThumbMax = (e: React.MouseEvent<HTMLInputElement>) => {
+    const percent = 100 - (+e.currentTarget.value / +e.currentTarget.max) * 100;
+    if (+e.currentTarget.value - this.state.minPrice < priceGap) {
+      this.setState({
+        ...this.state,
+        maxPrice: this.state.minPrice + priceGap,
+        isSmall: false
+      });
+    } else {
+      this.setState({
+        ...this.state,
+        maxPrice: +e.currentTarget.value,
+        isSmall: false,
+        progressBarStyle: {
+          ...this.state.progressBarStyle,
+          right: `${percent}%`
+        }
+      });
+    }
+  }
+
+  componentDidUpdate() {
+    this.props.changeMinPricePerRoomAction(this.state.minPrice);
+    this.props.changeMaxPricePerRoomAction(this.state.maxPrice);
+  }
+
   render(): React.ReactNode {
     return (
-      <div className="range-slider" >
-        <p className="caption">Range Slider</p>
-        <div className="range-slider__price-range">
-          <div className="range-slider__inputs">
+      <div className="range-price">
+        <div className="range-price__top">
+          <span className="caption">диапазон цены</span>
+          <div className="range-price__price-field">
             <input
-              id="input-0"
-              className="range-slider__input"
-              placeholder="4000₽"
+              className="range-price__input range-price__input-min"
+              name="price-min"
               type="number"
-              min={4000}
-              max={15000} />
-            <span>-</span>
+              min={0}
+              max={10000}
+              value={this.state.minPrice}
+              onChange={this.handleChangeMinPriceInput}
+            />
+            <span style={{ padding: '0 5px', }}>-</span>
             <input
-              id="input-1"
-              className="range-slider__input"
-              placeholder="15000₽"
+              className="range-price__input range-price__input-max"
+              name="price-max"
               type="number"
-              min={4000}
-              max={15000} />
+              min={0}
+              max={10000}
+              value={this.state.isSmall ? this.state.ifMaxIsSmall : this.state.maxPrice}
+              onChange={this.handleChangeMaxPriceInput}
+            />
           </div>
         </div>
-        <div
-          id="range-slider"
-          className="range-slider__slider"
-          ref={this.rangeSlider}
-        ></div>
+        <div id="range-slider" className="range-price__slider" >
+          <div
+            id="slider-progress"
+            className="range-price__slider-progress"
+            style={this.state.progressBarStyle}
+          ></div>
+          <div className="range-thumbs">
+            <input
+              className="range-thumbs__input range-thumbs__range-min"
+              type="range"
+              value={this.state.minPrice}
+              step={step}
+              min={0}
+              max={10000}
+              onInput={this.handleDragableThumbMin}
+            />
+            <input
+              className="range-thumbs__input range-thumbs__range-max"
+              type="range"
+              value={this.state.maxPrice}
+              step={step}
+              min={0}
+              max={10000}
+              onInput={this.handleDragableThumbMax}
+            />
+          </div>
+        </div>
       </div>
     );
   }
 }
+
+const mapDispatchToProps = (dispatch) => bindActionCreators({
+  changeMinPricePerRoomAction,
+  changeMaxPricePerRoomAction
+}, dispatch);
+
+export default connect(null, mapDispatchToProps)(RangeSlider);
